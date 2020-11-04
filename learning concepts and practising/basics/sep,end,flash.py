4b6ec16a187e2b414f07576d5b1cab57fda53a27
lovaraju987 <mailapallilovaraaju987@gmail.com>
2020-11-04 19:47:15 +0530

    practised ternary operators and sep,end arguments

---
+++ learning concepts and practising/basics/sep,end,flash.py
@@ -0,0 +1,8 @@
+''' sep, end, flash'''
+
+
+print('slfhs',2,'shalds',end = ' ') # by default print statement ends with \n(newline).so, this 'end' is used to change to ending of the print statement when we required it
+print('sfjsaa',3,'hissa')
+
+print('sfjsaa',2,'hissa',sep = ' ') # by default multiple statemnets in one print without any space. so the 'sep' seperates the multiple statements in print with given string by ou 
+print('sfjsaa',3,'hissa',sep = ' ')
---
+++ learning concepts and practising/basics/ternary_operators.py
@@ -0,0 +1,9 @@
+''' Ternary Operators'''
+a,b = 10,20
+x = a<b and a or b # print a when conditon is true. print b when condition is false
+print(x)
+
+'''
+drawback:
+In this mehtod values shouldn't be declared as 0 or false
+'''
