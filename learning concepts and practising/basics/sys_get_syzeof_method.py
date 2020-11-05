2bc41dd8eacf08211abcfedfef925ca0cef66145
lovaraju987 <mailapallilovaraaju987@gmail.com>
2020-11-05 13:42:33 +0530

    practiced some used methods

---
+++ learning concepts and practising/basics/sys_get_syzeof_method.py
@@ -0,0 +1,11 @@
+''' sys.getsizesf() method'''
+
+import sys
+
+x = 10
+y = [x,'raju',15]
+
+print(sys.getsizeof(x))  #this method is used to get sizeo of given object
+print(sys.getsizeof(y))
+
+
---
+++ learning concepts and practising/control structures/range_method.py
@@ -0,0 +1,4 @@
+''' range() method'''
+
+for x in range(2,10): #the range() method is used to iterate a certain number of times in for loops
+    print(x)
