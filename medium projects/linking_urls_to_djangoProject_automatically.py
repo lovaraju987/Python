53f232679cc3b96388ce8bdd6076fa527bf547f1
Lovaraju Mailapalli <mailapallilovaraaju987@gmail.com>
2020-11-26 11:09:07 +0530

    changes in linking urls django static files automatically

--- medium projects/linking_urls_to_djangoProject_automatically.py
+++ medium projects/linking_urls_to_djangoProject_automatically.py
@@ -1,14 +1,13 @@
 import re
 
-with open('index.html','r') as openIndex:
+with open('C:/Users/maila/Development/python_projects/learning python basics/learning concepts and practising/regular expressions/home.html','r') as openIndex:
    html = openIndex.read()
 
-reg = r'([a-zA-Z0-9-/.]+(\.js|\.jpg|\.png|\.html|\.gif|.css))'
+reg = r'([a-zA-Z0-9-/._:]+(\.js|\.jpg|\.png|\.html|\.gif|.css))'
 
 res = r"{% static '\1' %}"
 
 repl = re.sub(reg,res,html)
 print(repl)
-with open('htmltxt.html','w') as f:
+with open('C:/Users/maila/Development/python_projects/learning python basics/learning concepts and practising/regular expressions/home.html','w') as f:
    f.write(repl)
-
